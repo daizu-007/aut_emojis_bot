b094023f08d8cb2d2aeccb77ab3600b385ec1c46
daizu <128668088+daizu-007@users.noreply.github.com>
2023-10-28 23:11:29 +0900

    add function to make emojis from romaji

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # aut_emojis_bot
-:oharagi:シリーズを自動生成するDiscord Botです。  
+slackやdiscord、misskey等で見かける:igyo:、:tashikani:のような絵文字を自動で生成するdiscord botです。  
 `;{文字列};`というメッセージに反応して、{文字列}を絵文字に変換し、webhookで送信します。
+
+## クレジット
+このbotでは、ローマ字をひらがなに変換するために[Meatwo310/JapaneseRomajiConverter-Forge](<https://github.com/Meatwo310/JapaneseRomajiConverter-Forge>)の[romaji_to_hiragana.csv](<https://github.com/Meatwo310/JapaneseRomajiConverter-Forge/blob/1.19.2/src/main/resources/assets/japaneseromajiconverter/romaji_to_hiragana.csv>)と[romaji_to_hiragana2.csv](<https://github.com/Meatwo310/JapaneseRomajiConverter-Forge/blob/1.19.2/src/main/resources/assets/japaneseromajiconverter/romaji_to_hiragana_2.csv>)を辞書として使用しています。また、このmodでは[andree-surya/moji4j](<https://github.com/andree-surya/moji4j>)の[romaji_to_hiragana.csv](<https://github.com/andree-surya/moji4j/blob/ea0168f125da8791e951eab7cdf18b06a7db705b/src/main/resources/romaji_to_hiragana.csv>)を辞書のベースとして使用しています。[Meatwo310氏](<https://github.com/Meatwo310>)、[andree-surya氏](<https://github.com/andree-surya>)両名に深く感謝いたします。  
+それぞれのライセンスのコピーは以下をご覧ください。
+### Meatwo310/JapaneseRomajiConverter-Forge
+```
+Copyright 2023 Meatwo310
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+### andree-surya/moji4j
+```
+© Copyright 2016 Andree Surya
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
--- bot.py
+++ bot.py
@@ -6,22 +6,95 @@ import json
 from os import path
 import random
 import math
-#import textwrap
+import csv
+import requests
+import MeCab
 
 #あとから変更するかもしれない変数
 size = 128 #画像のサイズ
 
-temp_file = __file__[:-6] + "temp\emoji.png"
-print(temp_file)
-intents = discord.Intents.all()
-intents.messages = True
+#定数の設定
+this_dir = __file__[:-6] #このファイルのディレクトリ
+temp_file = this_dir + "temp\emoji.png"
+intents = discord.Intents.all() #botにすべての権限を与える
 bot = discord.Bot(intents=intents, command_prefix="/")
+romaji_to_japanese_map = {} #ローマ字ひらがな変換マップの定義
 
 #config.jsonから設定を読み込む
 with open(path.join(path.dirname(__file__), "config.json"), "r") as f:
     config = json.load(f)
 token = config["token"]
 font_path = config["font_path"]
+
+#ローマ字ひらがな変換表を読み込む
+def setup_romaji_map_from_resource_name(resource_name):
+    with open(resource_name, 'r', encoding='utf-8') as file:
+        csv_data = csv.reader(file) #csvファイルを読み込む
+        for line in csv_data: #csvファイルの各行について
+            if len(line) == 2: #行の要素数が2なら
+                romaji = line[0]
+                kana = line[1]
+                data = [kana, '0'] #ローマ字に対応するデータに[{ひらがな}, 0]を代入。0は移動量
+            elif len(line) == 3: #行の要素数が3なら
+                romaji = line[0]
+                kana = line[1]
+                offset = line[2] #行の3番目の要素を移動量とする
+                try:
+                    int(offset) #移動量が整数であることを確認
+                except ValueError:
+                    print(f"{resource_name}の{line}行目は不正です。")
+                    continue
+                data = [kana, offset] #ローマ字に対応するデータに[{ひらがな}, {移動量}]を代入
+            else: #行の要素数が2でも3でもないなら
+                if line != []: #空行でないならエラーを表示
+                    print(f"{resource_name}の{line}行目は不正です。")
+                continue
+            romaji_to_japanese_map[romaji] = data #ローマ字をキーとして、ローマ字に対応するデータを代入
+
+#ローマ字ひらがな変換表を読み込む
+setup_romaji_map_from_resource_name(path.join(path.dirname(__file__), "romaji_to_hiragana.csv"))
+setup_romaji_map_from_resource_name(path.join(path.dirname(__file__), "romaji_to_hiragana_2.csv"))
+
+#ローマ字をひらがなに変換する関数
+def convert_romaji_to_hiragana(romaji): #ローマ字をひらがなに変換する関数
+    romaji = romaji.lower() #ローマ字を小文字に変換
+    result = [] #結果を格納するリスト
+    i = 0 #処理しているローマ字のインデックス（何文字目かから1引いた数、１文字目は0）
+    while i < len(romaji): #変換する文字列の長さだけ繰り返す
+        found = False #該当するローマ字が見つかったかどうかを表す変数
+        for j in range(4, 0, -1): #4, 3, 2, 1の順にjを取る、多分総当り
+            if i + j <= len(romaji): #処理しようとする文字数がローマ字文字列の長さ以下なら
+                substring = romaji[i:i + j] #処理しようとする文字列を取得
+                if substring in romaji_to_japanese_map: #処理しようとする文字列がローマ字変換表のキーにあるなら 
+                    result.append(romaji_to_japanese_map[substring][0]) #結果のリストに変換後のひらがなを追加
+                    i += j + int(romaji_to_japanese_map[substring][1]) #処理している文字列に処理した量足して辞書の移動量を足す(多分負の数)
+                    found = True #該当するローマ字が見つかったことを表す変数をTrueにする
+                    break
+        else: #該当するローマ字が見つからなかったら
+            result.append(romaji[i]) #変換しない
+            i += 1 #インデックスを1増やす
+    return ''.join(result) #結果のリストを文字列に変換して返す
+
+#ひらがなを漢字に変換する関数
+def transliterate_hiragana_to_kanji(hiragana_text):
+    #パラメータを作成
+    parameters = {
+        'langpair': 'ja-Hira|ja',
+        'text': hiragana_text
+    }
+    # GETリクエストを送信してレスポンスを取得
+    response = requests.get('http://www.google.com/transliterate', params=parameters)
+    response_json = response.json()
+    # 変換結果を取得
+    result = []
+    for text in response_json: #結果のリストの各要素について（各要素は分割された入力文）
+        processed_text = text[1][0] #変換された文字列を取得
+        result.append(processed_text)
+    #リストを文字列にする
+    result = ''.join(result)
+    if result == "":
+        result = hiragana_text
+    return result
 
 #botが起動したときの処理
 @bot.event
@@ -36,21 +109,36 @@ async def on_message(message):
     text = message.content #メッセージの内容を取得
     if text.startswith(";") and text.endswith(";"):
         text = text[1:-1]
-        print(text)
+        #print(text)
         #await message.channel.send(file=discord.File(temp_file))
         if type(message.channel) == discord.TextChannel:
             webhooks = await message.channel.webhooks() #webhookの情報を取得
-            processed_webhooks = [obj for obj in webhooks if obj.name == "daizu-bot"]#webhookの情報からbotのwebhookの情報を取得
+            processed_webhooks = [obj for obj in webhooks if obj.name == "bot"]#webhookの情報からbotのwebhookの情報を取得
         else:
             processed_webhooks = []
         if processed_webhooks:
             webhook = processed_webhooks[0] #botのwebhookの情報を取得
+            text = text_processer(text) #テキストを処理
+            #print("絵文字にするテキスト: " + text)
             img = create_emoji(text) #絵文字を生成
             img.save(temp_file)
             await webhook.send(username=message.author.display_name, avatar_url=message.author.display_avatar, file=discord.File(temp_file))
             await message.delete()
         else:
             await message.reply("このチャンネルにはbotのwebhookがありません")
+
+#テキストを絵文字用に変換する関数
+def text_processer(text):
+    text = convert_romaji_to_hiragana(text) #ローマ字をひらがなに変換
+    if len(text) > 54: #テキストが54文字より長いなら
+        text_list = split_text(text) #54字ごとに分割。google CGI APIの制限
+        text = ""
+        for translating_text in text_list:
+            translated_text = transliterate_hiragana_to_kanji(translating_text) #ひらがなを漢字に変換
+            text += translated_text
+    else: #テキストが54文字以下なら
+        text = transliterate_hiragana_to_kanji(text) #ひらがなを漢字に変換
+    return text
 
 #絵文字を生成する関数
 def create_emoji(text):
@@ -123,14 +211,34 @@ def text_to_image(char,color=(0,0,0,255)):
     #画像を返す
     return img
 
+#テキストを形態素解析して分割する関数
+def split_text(text):
+    count_limit = 54
+    tagger = MeCab.Tagger("-Owakati") #Mecabで分かち書きできるようにする
+    splited_text = tagger.parse(text).split() #分かち書きする
+    result = []
+    while splited_text: #splited_textが空になるまで続ける
+        block = ""
+        if len(block)+len(splited_text[0]) < count_limit:
+            while len(block)+len(splited_text[0]) < count_limit: #blockにsplited_textの最初の単語を追加してもcount_limitを超えない間続ける
+                block += splited_text.pop(0) #splited_textの最初の単語をblockに追加して削除
+                if not splited_text: #splited_textが空になったらwhileを抜ける
+                    break
+        else:
+            block += splited_text.pop(0)
+        result.append(block)
+    return result
+
 #実行時に実行される処理
 if __name__ == "__main__":
+    print("botを実行します")
     bot.run(token) #botを実行
 
 #テスト
 #img = create_emoji("おはらぎ")
 #img = text_to_image("お")
 #img.save("C:/my projects/python/discord bots/aut_emojis_bot/test_images/test.png")
+#print(text_processer("korehakeitaisobunnsekiwofukumutekisutosyorizenntainotesutonotamenitukuraretabunnsyoudesu.monosugoityoubunnnitaisitedonoyounisyorisurunokakininarunodesugaro-majidetyoubunnwosakuseisurunohanakanakataihennnasagyoudesu.imanagareteiruonngakuhannanndesyouka?robottoda-tteitteimasune.sirabetemimasyouka?dannsurobottodannsutoiukyokurasiidesu.sorosorozisuutekinityoudoiidesukane?dounanndesyouka?"))
 
 
 """メモ
