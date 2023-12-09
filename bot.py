4c4f01d9186bbce13ea91239c68a42766c36cca1
daizu <128668088+daizu-007@users.noreply.github.com>
2023-12-10 00:18:31 +0900

    to use config

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import discord
 from PIL import Image, ImageDraw, ImageFont
 import json
 from os import path
-from os import environ
 import random
 import math
 import csv
@@ -34,9 +33,10 @@ intents = discord.Intents.all() #botにすべての権限を与える
 bot = discord.Bot(intents=intents, command_prefix="/")
 romaji_to_japanese_map = {} #ローマ字ひらがな変換マップの定義
 
-#環境変数からトークンを取得
-token = environ['AUT_EMOJIS_BOT_TOKEN']
-
+#config.jsonから設定を読み込む
+with open(path.join(path.dirname(__file__), "config.json"), "r") as f:
+    config = json.load(f)
+token = config["token"]
 
 #フォントのパスを取得
 font_path = path.join(path.dirname(__file__), "NotoSansJP-Bold.ttf")
